a805068197ace0a1c8c02a6c0edea1ff941a969c
franklin-saint <zeligbouillot@gmail.com>
2019-12-04 17:28:32 +0100

    Config devise mailers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,28 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-has_many  :classrooms
-has_many  :lessons, through: :classrooms
+  has_many  :classrooms
+  has_many  :lessons, through: :classrooms
 
-has_many :followed_lessons, foreign_key: "student_id", class_name: "Lesson"
-has_many :taught_lessons,  foreign_key: "teacher_id", class_name: "Lesson"
+  has_many :followed_lessons, foreign_key: "student_id", class_name: "Lesson"
+  has_many :taught_lessons,  foreign_key: "teacher_id", class_name: "Lesson"
 
 
-class Student < User
+
+  class Student < User
+    
+   
+  end 
+
+  class Teacher < User
+     
+    
+  end
 
 end 
-
-class Teacher < User 
-
-end
-
-
-
-
-
-
-
-
-
-
-end
