0ea82f655c6086cdcb372686739b904d15d1fb69
ThomasD972 <thomas.delage972@gmail.com>
2019-12-04 16:24:15 +0100

    Add student_id in model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,19 +12,12 @@ has_many :followed_lessons, foreign_key: "student_id", class_name: "Lesson"
 has_many :taught_lessons,  foreign_key: "teacher_id", class_name: "Lesson"
 
 
-class Student < User
-
-end 
+class Student < User 
+end
 
 class Teacher < User 
 
 end
-
-
-
-
-
-
 
 
 
