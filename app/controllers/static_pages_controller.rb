6ac0f80926cbb5b2ae089aa350946e3752b9aab4
Cano <barbara.cano.gaso@gmail.com>
2019-12-04 10:05:39 +0100

    creating the controller static pages as well as views

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+    def about
+    end
+    def contact
+    end
+    def faq
+    end
+    def legal_notice
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'lesson#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'test#index'
+  resources :lesson
+  resources :static_pages
+
 end
