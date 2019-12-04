48a649e06e3b5ef1e67b842f5e981f8efccddb40
Polo <paul.poitrenaud@gmail.com>
2019-12-04 18:02:47 +0100

    new lesson form up, soon working

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -1,4 +1,6 @@
 class LessonController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :create]
+
   def index
     @lessons = Lesson.all
     @users = User.all
@@ -6,5 +8,26 @@ class LessonController < ApplicationController
 
   def show
     @lesson = Lesson.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @lesson = Lesson.create(
+      title: params[:lesson_title], 
+      content: params[:lesson_content]) 
+    if @lesson.save 
+      flash[:success] = "Votre cours est en ligne !"
+      redirect_to root_path
+      puts "#"*10
+      puts "it worked"
+      puts "#"*10
+     else
+      render "new"
+      puts "#"*10
+      puts @lesson.errors
+      puts "#"*10
+    end
   end
 end
