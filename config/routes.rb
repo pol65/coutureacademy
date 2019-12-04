1b2f13920e2271e65c1ae45a6927b8f9fdf356fc
Cano <barbara.cano.gaso@gmail.com>
2019-12-04 10:52:27 +0100

    write sight contact us

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'lesson#index'
   devise_for :users
+  get 'static_pages/contact'
+  get 'static_pages/about'
+  get 'static_pages/faq'
+  get 'static_pages/legal_notice'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lesson
-  resources :static_pages
+
 
 end
