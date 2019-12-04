211577174a127892ab00f341c2f3bfbe84c1a9b7
BSarahB <bslajta@gmail.com>
2019-12-04 16:45:31 +0100

    setting rspec and testing rspec model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do 
+    @user = User.create(username: "ZelligBoGoss", email: "zelig@yopmail.com", description: "Le bogoss de la team bogoss")
+
+  context "validation" do
+
+    it "is valid with valid attributes" do
+      expect(@user).to be_a(User)
+      expect(@user).to be_valid
+    end
+  end
+end
 end
