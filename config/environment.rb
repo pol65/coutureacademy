981dd4f867343a78f9db3c4f0c9fbf8a4a8d3e6e
BSarahB <bslajta@gmail.com>
2019-12-05 19:44:32 +0100

    Sendgrid API working

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-ActionMailer::Base.smtp_settings =   {
-    :address            => 'smtp.gmail.com',
-    :port               => 587,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
-    :user_name          => ENV['GMAIL_LOGIN'],
-    :password           => ENV['GMAIL_PWD']
-  }
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
+  :domain => 'monsite.fr',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
