72ae07a2727cd3a9ef684e81ba49d81ee99b6dac
ThomasD972 <thomas.delage972@gmail.com>
2019-12-05 16:22:31 +0100

    Dashboard admin more DRY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def is_admin?
+        if current_user.admin?
+        else
+            redirect_to root_path
+            flash[:danger] = "Not an admin"
+        end
+    end
 end
