e83dcfbbcefc8b49bffd1f91a6dcba737f9139b5
Polo <paul.poitrenaud@gmail.com>
2019-12-06 13:08:46 +0100

    conflicts solved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
@@ -9,14 +8,4 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     end
-=======
-
-    before_action :configure_devise_parameters, if: :devise_controller?
-
-    def configure_devise_parameters
-      devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :description, :email, :password, :password_confirmation)}
-      devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :description, :email, :password, :password_confirmation)}
-    end
-
->>>>>>> development
 end
