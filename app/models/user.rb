ac969c81bf68306999aeedd38ae6920f9679312e
BSarahB <bslajta@gmail.com>
2019-12-10 10:32:04 +0100

    adding fb providers to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
-
+    devise :omniauthable, omniauth_providers: [:facebook]
 
 has_many  :classrooms
 has_many  :lessons, through: :classrooms
