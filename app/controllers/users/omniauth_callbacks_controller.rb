d2ba5548a545779aff333df6e406b5f207f3bd72
BSarahB <bslajta@gmail.com>
2019-12-10 11:17:46 +0100

    creating def facebook in controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 def facebook
 
 	@user = User.from_facebook(request.env['omniauth.auth'])
+	if @user.persisted?
+		sign_in_and_redirect @user, event: :authentification
 
+	else
+		session['devise.facebook'] = request.env['omniauth.auth']
+		redirect_to new_user_registration_url
+	end
 end
 end
