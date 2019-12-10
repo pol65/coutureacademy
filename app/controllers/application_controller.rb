9d5c4a684e3a4917ffe4a1507a031b8af2254801
Cano <barbara.cano.gaso@gmail.com>
2019-12-10 15:25:39 +0100

    using asset routes for images but not all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   include ApplicationHelper
 
     before_action :configure_permitted_parameters, if: :devise_controller?
@@ -6,7 +7,7 @@ class ApplicationController < ActionController::Base
     protected
   
     def configure_permitted_parameters
-      added_attrs = [:username, :description, :email, :password, :password_confirmation]
+      added_attrs = [:username, :avatar, :description, :email, :password, :password_confirmation]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-    def show
+    def show    
         @lessons = Lesson.where(:category => params[:id]).all
     end
 end
