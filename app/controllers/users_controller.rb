dd3ac4660b424c1d022c7b0bb651eb59fd486e6a
ThomasD972 <thomas.delage972@gmail.com>
2019-12-10 14:48:23 +0100

    Add link to taught_lesson for teacher

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,5 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @classrooms = Classroom.where(:student_id => current_user.id)
     @taught_lessons = Lesson.where(:teacher_id => current_user.id)
-    
     end
 end
