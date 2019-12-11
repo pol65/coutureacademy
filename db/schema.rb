cb40cfbaa55f0d8f77fbeba7d5f3e8d584dd59e4
ThomasD972 <thomas.delage972@gmail.com>
2019-12-11 11:19:03 +0100

    Migration checked lesson and validation checked

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_091932) do
+ActiveRecord::Schema.define(version: 2019_12_11_094812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_091932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "summary"
+    t.boolean "checked", default: false
     t.index ["teacher_id"], name: "index_lessons_on_teacher_id"
   end
 
