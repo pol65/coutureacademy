c942976453cfae723568d6d5dac474096531ab48
franklin-saint <zeligbouillot@gmail.com>
2019-12-12 16:08:26 +0100

    errors message

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,10 +8,10 @@ class Lesson < ApplicationRecord
     has_many :comments
 
     validates :title, presence: {message: "Le titre est nécessaire à la création d'un cours"}
+    validates :title, uniqueness: {message: "Ce titre est déja pris, soyez unique"}
+    validates :title , length: { in: 6..50 }
     validates :content, presence: true 
     validates :category, presence: true 
-    validates :title, uniqueness: true 
-    validates :title , length: { in: 6..50 }
     validates :content, length: {minimum: 50 }
 
 end
