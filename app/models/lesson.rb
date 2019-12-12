363ccbddd1885c7d7336011dd56c8b19148d4239
franklin-saint <zeligbouillot@gmail.com>
2019-12-12 16:23:40 +0100

    add error message validates content

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -10,8 +10,9 @@ class Lesson < ApplicationRecord
     validates :title, presence: {message: "Le titre est nécessaire à la création d'un cours"}
     validates :title, uniqueness: {message: "Ce titre est déja pris, soyez unique"}
     validates :title , length: { in: 6..50 ,message: "Le titre doit contenir entre 6 et 50 caractères"}
-    validates :content, presence: true 
+    validates :content, presence: {message: "Un contenu est nécessaire à la création d'un cours"}
+    validates :content, length: {minimum: 50 ,message: "Le contenu doit contenir au minimum 50 caractères"}
     validates :category, presence: true 
-    validates :content, length: {minimum: 50 }
+    
 
 end
